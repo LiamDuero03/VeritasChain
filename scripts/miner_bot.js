f9ba97ad957ea00c791f6d5573fae04fb9c74490
LiamDuero03 <liam.duero@gmail.com>
2025-12-06 16:09:20 +0000

    Running PoC

--- scripts/miner_bot.js
+++ scripts/miner_bot.js
@@ -1,9 +1,9 @@
 const hre = require("hardhat");
 
 // !!! UPDATE THESE AFTER DEPLOYMENT !!!
-const TOKEN_ADDR = "PUT_YOUR_TOKEN_ADDRESS_HERE"; 
-const REGISTRY_ADDR = "PUT_YOUR_REGISTRY_ADDRESS_HERE";
-const CORE_ADDR = "PUT_YOUR_CORE_ADDRESS_HERE";
+const TOKEN_ADDR = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; 
+const REGISTRY_ADDR = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const CORE_ADDR = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
 async function main() {
     // We skip the first 2 accounts (Deployer and Aggregator)
--- scripts/user_submit.js
+++ scripts/user_submit.js
@@ -1,8 +1,8 @@
 const hre = require("hardhat");
 
 // !!! UPDATE THESE AFTER DEPLOYMENT !!!
-const CORE_ADDR = "PUT_YOUR_CORE_ADDRESS_HERE"; 
-const TOKEN_ADDR = "PUT_YOUR_TOKEN_ADDRESS_HERE";
+const CORE_ADDR = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"; 
+const TOKEN_ADDR = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 async function main() {
     const [deployer, aggregator, m1, m2, m3, user] = await hre.ethers.getSigners();
